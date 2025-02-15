b3d09dca442c8877c12f26cdf0b418ad7c2e4a21
Antik <120013201+NewAntik@users.noreply.github.com>
2025-02-15 15:07:55 +0200

    Load context test has been added.

---
+++ src/test/java/ai/facie/tradedatatask/TradeDataTaskApplicationTests.java
@@ -0,0 +1,12 @@
+package ai.facie.tradedatatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeDataTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
