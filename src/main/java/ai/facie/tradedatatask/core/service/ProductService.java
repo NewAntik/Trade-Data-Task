a0708420a631db1330b655748f7db81fe6822806
Antik <120013201+NewAntik@users.noreply.github.com>
2025-02-16 19:06:50 +0200

    batching logic for redis has been added.

--- src/main/java/ai/facie/tradedatatask/core/service/ProductService.java
+++ src/main/java/ai/facie/tradedatatask/core/service/ProductService.java
@@ -1,10 +1,13 @@
 package ai.facie.tradedatatask.core.service;
 
 import java.io.InputStream;
+import java.util.List;
 
 public interface ProductService {
 
 	void loadProductsFromStream(InputStream stream);
 
 	String getProductName(Long productId);
+
+	List<String> getProductNamesInBatch(List<String> productIds);
 }
